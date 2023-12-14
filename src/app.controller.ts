e8a5373ebd3f34efcc7216a67ba29d0ef3a01354
LÊ Y KHOA <91020137+leykhoa@users.noreply.github.com>
2023-12-14 14:50:56 +0700

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { UploadFile } from './common/middlewares/uploadFile.middleware';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello world!!!';
   }
 }
